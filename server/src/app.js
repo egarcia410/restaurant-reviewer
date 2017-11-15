e753020c78b5ca8a1eb9d46668d447512de371fc
egarcia410 <egarcia410@gmail.com>
2017-11-15 17:14:59 -0600

    Update: app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,2 +1,12 @@
 const express = require('express');
+const knex = require('./db/knex');
+
 const app = express();
+
+app.get('/status', (req, res) => {
+    res.send({
+        message: 'Hello'
+    })
+})
+
+app.listen(process.env.port || 8081)
