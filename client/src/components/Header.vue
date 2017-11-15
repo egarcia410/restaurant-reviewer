00711fcaee30a394828a92dd07a8743c36969bd4
egarcia410 <egarcia410@gmail.com>
2017-11-15 15:38:55 -0600

    Create: Search Component

---
+++ client/src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <router-link class="navbar-item" to="/">Home</router-link>
+                <!-- <a class="navbar-item">Home</a> -->
+            </div>
+            <div class="navbar-menu navbar-end">
+                <router-link class="navbar-item" to="/search">Search</router-link>
+                <router-link class="navbar-item" to="/create">Create</router-link>
+            </div>
+        </nav>
+    </div>
+</template>
